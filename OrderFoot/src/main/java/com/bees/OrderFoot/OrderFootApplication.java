3b45fdc4a1c24832453c01b01334d100861ce44c
vuhongthien <vuhongthien18@gmail.com>
2022-09-20 16:47:55 +0700

    Project

---
+++ OrderFoot/src/main/java/com/bees/OrderFoot/OrderFootApplication.java
@@ -0,0 +1,13 @@
+package com.bees.OrderFoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFootApplication.class, args);
+	}
+
+}
